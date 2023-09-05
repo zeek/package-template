6dc22194d3895d42f3ff88a6055cc6f41cade89c
Arne Welzel <arne.welzel@corelight.com>
2023-09-05 10:24:17 +0200

    tests: canonify-template-info for more versions
    Oh, baseline joy.

--- tests/scripts/canonify-template-info.sh
+++ tests/scripts/canonify-template-info.sh
@@ -2,4 +2,6 @@
 
 cat |
     sed 's/author:.*/author: AUTHOR/' |
-    sed 's/origin:.*/origin: ORIGIN/'
+    sed 's/origin:.*/origin: ORIGIN/' |
+    # versions: v0.99.0, v1.0.0, v2.0.0
+    sed -r 's/versions: (v[.0-9]+(, )?)+/version: VERSIONS/'
